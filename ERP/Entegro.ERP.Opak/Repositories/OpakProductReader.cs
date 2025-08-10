d7031171e35f96399ab66b037b4723c4a458229a
FATIH <fatihkokay@live.com>
2025-08-10 23:34:24 +0300

    edit

---
+++ ERP/Entegro.ERP.Opak/Repositories/OpakProductReader.cs
@@ -0,0 +1,18 @@
+﻿using Entegro.ERP.Abstractions.DTOs;
+using Entegro.ERP.Abstractions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entegro.ERP.Opak.Repositories
+{
+    public class OpakProductReader : IErpProductReader
+    {
+        public Task<IEnumerable<ProductDto>> GetProductsAsync(int page, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
