4750195c0d915f63d7bd5958857f5c02594069dc
FATIH <fatihkokay@live.com>
2025-08-10 15:59:02 +0300

    edit

--- Entegro.Application/DTOs/Category/CategoryDto.cs
+++ Entegro.Application/DTOs/Category/CategoryDto.cs
@@ -8,5 +8,16 @@ namespace Entegro.Application.DTOs.Category
 {
     public class CategoryDto
     {
+        public int Id { get; set; }
+        public int ParentCategoryId { get; set; }
+        public string TreePath { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? MetaTitle { get; set; }
+        public string? MetaDescription { get; set; }
+        public string? MetaKeywords { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
     }
 }
--- Entegro.Application/DTOs/Category/CreateCategoryDto.cs
+++ Entegro.Application/DTOs/Category/CreateCategoryDto.cs
@@ -8,5 +8,15 @@ namespace Entegro.Application.DTOs.Category
 {
     public class CreateCategoryDto
     {
+        public int ParentCategoryId { get; set; }
+        public string TreePath { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? MetaTitle { get; set; }
+        public string? MetaDescription { get; set; }
+        public string? MetaKeywords { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
     }
 }
