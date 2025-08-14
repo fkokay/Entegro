ac49819a6a928fe1cb59c7c95a58868ae1e64453
FATIH <fatihkokay@live.com>
2025-08-14 09:58:07 +0300

    edit

---
+++ Entegro.Web/Controllers/IdentityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entegro.Web.Controllers
+{
+    public class IdentityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
