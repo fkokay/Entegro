5ae01b14213a2d58c83001b57350c3c044a836eb
FATIH <fatihkokay@live.com>
2025-08-15 13:56:41 +0300

    edit

---
+++ Entegro.Web/Controllers/ProductAttributeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entegro.Web.Controllers
+{
+    public class ProductAttributeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
