70c48c0753e52eb2e4107085f09e7bf99557a6fc
FATIH <fatihkokay@live.com>
2025-08-15 12:54:46 +0300

    edit

---
+++ Entegro.Domain/Entities/ProductAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Entegro.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entegro.Domain.Entities
+{
+    [Table("ProductAttribute")]
+    public class ProductAttribute : BaseEntity, IDisplayOrder
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
