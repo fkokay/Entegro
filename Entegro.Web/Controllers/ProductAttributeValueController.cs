3571ffa3a0723eb4a20e0ef462a3d0a274acc103
Emre <Emre@DESKTOP-0C8P603>
2025-08-18 16:45:49 +0300

    ##

--- Entegro.Web/Controllers/ProductAttributeValueController.cs
+++ Entegro.Web/Controllers/ProductAttributeValueController.cs
@@ -1,4 +1,5 @@
-﻿using Entegro.Application.Interfaces.Services;
+﻿using Entegro.Application.DTOs.ProductAttributeValue;
+using Entegro.Application.Interfaces.Services;
 using Entegro.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,9 +8,11 @@ namespace Entegro.Web.Controllers
     public class ProductAttributeValueController : Controller
     {
         private readonly IProductAttributeValueService _productAttributeValueService;
-        public ProductAttributeValueController(IProductAttributeValueService productAttributeValueService)
+        private readonly IProductAttributeService _productAttributeService;
+        public ProductAttributeValueController(IProductAttributeValueService productAttributeValueService, IProductAttributeService productAttributeService)
         {
             _productAttributeValueService = productAttributeValueService ?? throw new ArgumentNullException(nameof(productAttributeValueService));
+            _productAttributeService = productAttributeService;
         }
         public IActionResult Index()
         {
@@ -20,7 +23,58 @@ namespace Entegro.Web.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductAttributeValueViewModel model)
+        {
+            var createDto = new CreateProductAttributeValueDto
+            {
+                Name = model.Name,
+                DisplayOrder = model.DisplayOrder,
+                ProductAttributeId = model.ProductAttributeId
+            };
+            await _productAttributeValueService.AddAsync(createDto);
 
+            return Json(new { success = true });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var productAttributeValue = await _productAttributeValueService.GetByIdAsync(id);
+            if (productAttributeValue == null)
+            {
+                return NotFound();
+            }
+
+            var productAttributeValueModel = new ProductAttributeValueViewModel
+            {
+                ProductAttributeId = productAttributeValue.ProductAttributeId,
+                DisplayOrder = productAttributeValue.DisplayOrder,
+                Name = productAttributeValue.Name,
+                Id = productAttributeValue.Id
+            };
+
+
+            return Json(productAttributeValueModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UpdateProductAttributeValueViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var updateDto = new UpdateProductAttributeValueDto
+                {
+                    Id = model.Id,
+                    Name = model.Name,
+                    ProductAttributeId = model.ProductAttributeId,
+                    DisplayOrder = model.DisplayOrder
+                };
+                await _productAttributeValueService.UpdateAsync(updateDto);
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
         [HttpPost]
         public async Task<IActionResult> ProductAttributeValueList([FromBody] GridCommand model)
         {
@@ -39,6 +93,24 @@ namespace Entegro.Web.Controllers
                 data = result.Items
             });
 
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var isSuccess = await _productAttributeValueService.DeleteAsync(id);
+            if (isSuccess)
+            {
+                return Json(new { success = true });
+            }
+            return Json(new { success = false, message = "Silinecek Varyant Değeri Bulunamadı" });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProductAttribute()
+        {
+            var data = await _productAttributeService.GetAllAsync();
+            var results = data.Select(d => new { id = d.Id, text = d.Name, });
+            return Json(new { results });
         }
     }
 }
---
+++ Entegro.Web/Models/ProductAttributeValueViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Entegro.Web.Models
+{
+    public class ProductAttributeValueViewModel
+    {
+        public int Id { get; set; }
+        public int ProductAttributeId { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+    public class CreateProductAttributeValueViewModel
+    {
+        public int ProductAttributeId { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+    public class UpdateProductAttributeValueViewModel
+    {
+        public int Id { get; set; }
+        public int ProductAttributeId { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
