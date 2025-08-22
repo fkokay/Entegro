bd9115c750668599f4d29760c5f74d1e236d7ece
FATIH <fatihkokay@live.com>
2025-08-22 12:39:18 +0300

    edit

---
+++ Entegro.Domain/Entities/ProductIntegration.cs
@@ -0,0 +1,19 @@
+﻿using Entegro.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entegro.Domain.Entities
+{
+    [Table("ProductIntegration")]
+    public class ProductIntegration : BaseEntity
+    {
+        public int ProductId { get; set; }
+        public int IntegrationSystemId { get; set; }
+        public DateTime? LastSyncDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
