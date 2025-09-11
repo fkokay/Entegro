3ac805499b8a45c4e312af4953b88a163319e22c
Emre <Emre@DESKTOP-0C8P603>
2025-09-11 10:25:24 +0300

    #

--- Entegro.Web/Program.cs
+++ Entegro.Web/Program.cs
@@ -1,22 +1,10 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Entegro;
-using Entegro.Application.Events;
-using Entegro.Application.Interfaces;
-using Entegro.Application.Interfaces.Repositories;
-using Entegro.Application.Interfaces.Services;
-using Entegro.Application.Interfaces.Services.Commerce;
-using Entegro.Application.Interfaces.Services.Marketplace;
 using Entegro.Application.Mappings;
-using Entegro.Application.Services;
-using Entegro.Application.Services.Commerce;
-using Entegro.Application.Services.Commerce.Smartstore;
-using Entegro.Application.Services.Marketplace;
-using Entegro.Domain.Entities.Catalog;
 using Entegro.Engine;
 using Entegro.Infrastructure.Data;
-using Entegro.Infrastructure.EventBus;
-using Entegro.Infrastructure.Repositories;
+using Entegro.Infrastructure.Extensions;
 using Entegro.Utilities;
 using Mapster;
 using MapsterMapper;
@@ -36,7 +24,7 @@ using System.Text.RegularExpressions;
 var rgSystemSource = new Regex("^File|^System|^Microsoft|^Serilog|^Autofac|^Castle|^MiniProfiler|^Newtonsoft|^Pipelines|^Azure|^StackExchange|^Superpower|^Dasync", RegexOptions.Compiled);
 var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? Environments.Production;
 var isDevEnvironment = IsDevEnvironment();
-var baseDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+var baseDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
 
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
@@ -44,12 +32,7 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
     ContentRootPath = isDevEnvironment ? null : baseDirectory
 });
 
-builder.Services.AddControllersWithViews().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-    options.JsonSerializerOptions.PropertyNamingPolicy = null;
-});
-
+#region Logging
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .WriteTo.Console()
@@ -63,17 +46,19 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 builder.Host.UseSerilog();
+#endregion
 
-builder.Services.AddCors(options =>
+#region MVC + JSON
+builder.Services.AddControllersWithViews().AddJsonOptions(options =>
 {
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-    });
+    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
 });
+#endregion
 
+#region Authentication & Authorization
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+.AddCookie(options =>
 {
     options.Cookie.Name = "EntegroAuthentication";
     options.CookieManager = new ChunkingCookieManager();
@@ -87,92 +72,42 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     options.SlidingExpiration = true;
     options.Cookie.MaxAge = options.ExpireTimeSpan;
 });
-
 builder.Services.AddAuthorization();
+#endregion
 
+#region EF Core
 builder.Services.AddDbContext<EntegroDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
     options.UseLazyLoadingProxies();
 });
-var configuration = (IConfiguration)builder.Configuration;
+#endregion
 
-
-builder.Host
-    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-    .UseSerilog(dispose: true);
-
+#region Autofac & Engine
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+             .UseSerilog(dispose: true);
 
 var startupLogger = new SerilogLoggerFactory(Log.Logger).CreateLogger("File");
-var appContext = new SmartApplicationContext(builder.Environment, configuration, startupLogger);
+var appContext = new SmartApplicationContext(builder.Environment, builder.Configuration, startupLogger);
 var engine = EngineFactory.Create(appContext.AppConfiguration);
 var engineStarter = engine.Start(appContext);
 
-// Configure RequestSizeLimit and RequestFormLimits
 if (appContext.AppConfiguration.MaxRequestBodySize != null)
 {
     builder.WebHost.ConfigureKestrel(kestrel =>
     {
         kestrel.Limits.MaxRequestBodySize = appContext.AppConfiguration.MaxRequestBodySize;
     });
-
     builder.Services.Configure<FormOptions>(form =>
     {
         form.MultipartBodyLengthLimit = appContext.AppConfiguration.MaxRequestBodySize.Value;
     });
 }
-
-// Add NativeLibraryDirectory to PATH environment variable
 AddPathToEnv(appContext.RuntimeInfo.NativeLibraryDirectory);
 
-// Add services to the container.
 engineStarter.ConfigureServices(builder.Services);
-
-// Add services to the Autofac container.
 builder.Host.ConfigureContainer<ContainerBuilder>(engineStarter.ConfigureContainer);
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
-
-builder.Services.AddScoped<IBrandRepository, BrandRepository>();
-builder.Services.AddScoped<IBrandService, BrandService>();
-
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-
-builder.Services.AddScoped<IProductCategoryMappingRepository, ProductCategoryMappingRepository>();
-builder.Services.AddScoped<IProductCategoryMappingService, ProductCategoryMappingService>();
-
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-
-builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
-builder.Services.AddScoped<IOrderItemService, OrderItemService>();
-
-builder.Services.AddScoped<IMediaFolderRepository, MediaFolderRepository>();
-builder.Services.AddScoped<IMediaFolderService, MediaFolderService>();
-
-
-builder.Services.AddScoped<IMediaFileRepository, MediaFileRepository>();
-builder.Services.AddScoped<IMediaFileService, MediaFileService>();
-
-
-builder.Services.AddScoped<IProductAttributeRepository, ProductAttributeRepository>();
-builder.Services.AddScoped<IProductAttributeService, ProductAttributeService>();
-
-builder.Services.AddScoped<IProductAttributeValueRepository, ProductAttributeValueRepository>();
-builder.Services.AddScoped<IProductAttributeValueService, ProductAttributeValueService>();
-
-builder.Services.AddScoped<IProductVariantAttributeRepository, ProductVariantAttributeRepository>();
-builder.Services.AddScoped<IProductVariantAttributeService, ProductVariantAttributeService>();
-
-builder.Services.AddScoped<IProductVariantAttributeValueRepository, ProductVariantAttributeValueRepository>();
-builder.Services.AddScoped<IProductVariantAttributeValueService, ProductVariantAttributeValueService>();
-
-builder.Services.AddScoped<IProductAttributeFormatter, ProductAttributeFormatter>();
-
-builder.Services.AddScoped<IProductImageMappingRepository, ProductImageMappingRepository>();
-builder.Services.AddScoped<IProductImageMappingService, ProductImageMappingService>();
+#endregion
 
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
@@ -220,10 +155,6 @@ builder.Services.AddScoped<ISpecificationAttributeRepository, SpecificationAttri
 builder.Services.AddScoped<ISpecificationAttributeService, SpecificationAttributeService>();
 
 
-builder.Services.AddScoped<IProductSpecificationAttributeMappingRepository, ProductSpecificationAttributeMappingRepository>();
-builder.Services.AddScoped<IProductSpecificationAttributeMappingService, ProductSpecificationAttributeMappingService>();
-
-
 builder.Services.AddScoped<IEmailAccountRepository, EmailAccountRepository>();
 builder.Services.AddScoped<IEmailAccountService, EmailAccountService>();
 
@@ -235,45 +166,27 @@ builder.Services.AddScoped<ITreeNodeRepository<Category>, TreeNodeRepository<Cat
 
 builder.Services.AddScoped<ISmartstoreService, SmartstoreService>();
 
-
-builder.Services.AddScoped<IEventPublisher, EventBus>();
-builder.Services.AddScoped<SmartstoreClient>();
-
-builder.Services.AddScoped<ICommerceProductWriter, SmartstoreProductWriter>();
-builder.Services.AddScoped<IEventHandler<ProductIntegrationRecordUpdatedEvent>, SmartstoreProductWriter>();
-
-builder.Services.AddScoped<ITrendyolService, TrendyolService>();
-builder.Services.AddScoped<IEventHandler<ProductIntegrationRecordUpdatedEvent>, TrendyolService>();
-
-builder.Services.AddScoped<IN11Service, N11Service>();
-builder.Services.AddScoped<IEventHandler<ProductIntegrationRecordUpdatedEvent>, N11Service>();
-
-builder.Services.AddHttpClient();
-
 MapsterConfig.RegisterMappings();
 builder.Services.AddSingleton(TypeAdapterConfig.GlobalSettings);
 builder.Services.AddScoped<IMapper, ServiceMapper>();
+#endregion
 
+#region Build App
 var app = builder.Build();
-var providerContainer = appContext as IServiceProviderContainer;
-providerContainer.ApplicationServices = app.Services;
+(appContext as IServiceProviderContainer)!.ApplicationServices = app.Services;
 
-// At this stage we can set the scoped service container.
 engine.Scope = new ScopedServiceContainer(
     app.Services.GetRequiredService<ILifetimeScopeAccessor>(),
     app.Services.GetRequiredService<IHttpContextAccessor>(),
     app.Services.AsLifetimeScope());
 
-// Build request pipeline
 app.Lifetime.ApplicationStarted.Register(() =>
 {
     appContext.Freeze();
     engineStarter.Dispose();
-    engineStarter = null;
 });
 
 var supportedCultures = new[] { new CultureInfo("en-US") };
-
 app.UseRequestLocalization(new RequestLocalizationOptions
 {
     DefaultRequestCulture = new RequestCulture("en-US"),
@@ -281,26 +194,21 @@ app.UseRequestLocalization(new RequestLocalizationOptions
     SupportedUICultures = supportedCultures
 });
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 
 using (var scope = app.Services.CreateScope())
 {
@@ -309,33 +217,25 @@ using (var scope = app.Services.CreateScope())
 }
 
 engineStarter.ConfigureApplication(app);
-
 app.Run();
+#endregion
 
-
+#region Helpers
 bool IsDevEnvironment()
 {
-    if (environmentName == Environments.Development)
-        return true;
-
-    if (System.Diagnostics.Debugger.IsAttached)
-        return true;
-
-    // if there's a 'Smartstore.sln' in one of the parent folders,
-    // then we're likely in a dev environment
-    if (CommonHelper.FindSolutionRoot(Directory.GetCurrentDirectory()) != null)
-        return true;
-
+    if (environmentName == Environments.Development) return true;
+    if (System.Diagnostics.Debugger.IsAttached) return true;
+    if (CommonHelper.FindSolutionRoot(Directory.GetCurrentDirectory()) != null) return true;
     return false;
 }
 void AddPathToEnv(string path)
 {
     var name = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Path" : "PATH";
     var value = Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
-
-    if (value.IsEmpty() || !value.Contains(path))
+    if (string.IsNullOrWhiteSpace(value) || !value.Contains(path))
     {
-        value = value.EmptyNull().Trim(';') + ';' + path;
+        value = (value ?? "").Trim(';') + ';' + path;
         Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.Process);
     }
 }
+#endregion
