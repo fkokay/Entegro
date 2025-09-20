152b5565dbdab5a7868090e43147985664bc6718
FATIH <fatihkokay@live.com>
2025-09-21 00:17:19 +0300

    edit

---
+++ Entegro.Web/Components/DashboardViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entegro.Web.Components
+{
+    public class DashboardViewComponent : ViewComponent
+    {
+        public DashboardViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke(string dashboardType)
+        {
+            return View(dashboardType);
+        }
+    }
+}
