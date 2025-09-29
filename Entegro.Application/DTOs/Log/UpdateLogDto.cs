cfb404abb7f1df1b181cbb796acfd154700cd087
Emre <Emre@DESKTOP-0C8P603>
2025-09-29 11:30:43 +0300

    edit

---
+++ Entegro.Application/DTOs/Log/UpdateLogDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Entegro.Application.DTOs.Log
+{
+    public class UpdateLogDto
+    {
+        public int Id { get; set; }
+        public string? Level { get; set; }
+        public string? Message { get; set; }
+        public string? MessageTemplate { get; set; }
+        public DateTimeOffset TimeStamp { get; set; }
+        public string? Exception { get; set; }
+        public string? Properties { get; set; }
+        public string? LogEvent { get; set; }
+    }
+}
