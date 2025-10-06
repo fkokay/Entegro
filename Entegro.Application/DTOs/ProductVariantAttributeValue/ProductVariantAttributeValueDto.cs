9d837c59865119e9d6375111970f7beb669fb8b2
Emre <Emre@DESKTOP-0C8P603>
2025-10-06 10:03:39 +0300

    #

--- Entegro.Application/DTOs/ProductVariantAttributeValue/ProductVariantAttributeValueDto.cs
+++ Entegro.Application/DTOs/ProductVariantAttributeValue/ProductVariantAttributeValueDto.cs
@@ -1,6 +1,4 @@
-﻿using Entegro.Application.DTOs.ProductVariantAttribute;
-
-namespace Entegro.Application.DTOs.ProductVariantAttributeValue
+﻿namespace Entegro.Application.DTOs.ProductVariantAttributeValue
 {
     public class ProductVariantAttributeValueDto
     {
@@ -8,5 +6,6 @@ namespace Entegro.Application.DTOs.ProductVariantAttributeValue
         public int EntityId { get; set; }
         public int ProductVariantAttributeId { get; set; }
         public string Name { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
